00a84a1b70a28fbb61659677cd5d21d22e555898
Mr Lu <lxxlxx888@163.com>
2017-01-12 22:09:07 +0800

    fifth commit 使用了okHttp3来进行网络请求，线程间同步仍然存在问题

--- app/src/main/java/com/rssreader/mrlu/myrssreader/View/ListActivity.java
+++ app/src/main/java/com/rssreader/mrlu/myrssreader/View/ListActivity.java
@@ -2,6 +2,8 @@ package com.rssreader.mrlu.myrssreader.View;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,9 +16,13 @@ import com.rssreader.mrlu.myrssreader.Model.RSSFeed;
 import com.rssreader.mrlu.myrssreader.Model.RSSHandler;
 import com.rssreader.mrlu.myrssreader.Model.RSSItem;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.xml.parsers.SAXParser;
@@ -34,7 +40,17 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
     public final String tag = "RSSReader";
     private RSSFeed feed = null;
 
+    InputStream is;
     OkHttpClient mOkHttpClient;
+    InputSource isc;
+
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +60,6 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
         feed = getFeed(RSS_URL);
 
 
-
         showListView();
     }
 
@@ -52,7 +67,7 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
     private RSSFeed getFeed(String urlString) {
         try {
 
-            URL url = new URL(urlString);
+            final URL url = new URL(urlString);
 
             //新建SAX--xml解析工厂类
             SAXParserFactory factory = SAXParserFactory.newInstance();
@@ -64,67 +79,89 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
             RSSHandler rssHander = new RSSHandler();
 
 
-            ///okhttp3 网络连接
-
-            mOkHttpClient = new OkHttpClient();
-            Request.Builder requestBuilder = new Request.Builder().url(url);
-
-            //可以省略，默认是GET请求
-            requestBuilder.method("GET", null);
-            Request request = requestBuilder.build();
-            Call mcall = mOkHttpClient.newCall(request);
-            mcall.enqueue(new Callback() {
+            new Thread() {
                 @Override
-                public void onFailure(Call call, IOException e) {
-                    Log.e("netWork", e.toString());
+                public void run() {
+                    ///okhttp3 网络连接
+
+                    mOkHttpClient = new OkHttpClient();
+                    Request.Builder requestBuilder = new Request.Builder().url(url);
+
+                    //可以省略，默认是GET请求
+                    requestBuilder.method("GET", null);
+                    Request request = requestBuilder.build();
+                    Call mcall = mOkHttpClient.newCall(request);
+                    mcall.enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Log.e("netWorkFailure", e.toString());
+
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+
+                            if (response.isSuccessful()) {
+
+                                Log.i("responeSuccess", "返回成功了");
+
+                                String str4 = response.body().string();
+                                Log.i("body", "str4---" + str4);
+
+                                is = response.body().byteStream();
+
+                      /*  if (null != response.cacheResponse()) {
+                            String str = response.cacheResponse().toString();
+                            Log.i("cache", "cache---" + str);
+
+                            String str2 = response.body().toString();
+                            Log.i("body", "str2---" + str2);
+
+                            is = response.body().byteStream();
+
+                        } else {
+                            response.body().string();
+                            String str = response.networkResponse().toString();
+                            Log.i("network", "network---" + str);
+
+                            String str2 = response.networkResponse().body().toString();
+                            Log.i("network", "str2---" + str2);
+
+                            String str3 = response.body().toString();
+                            Log.i("body", "str3---" + str3);
+
+                        }*/
+
+                            } else {
+                                System.out.println("返回失败");
+                            }
+                        }
+
+                    });
 
                 }
+            }.start();
 
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    if (null != response.cacheResponse()) {
-                        String str = response.cacheResponse().toString();
-                        Log.i("wangshu", "cache---" + str);
-                    } else {
-                        response.body().string();
-                        String str = response.networkResponse().toString();
-                        Log.i("wangshu", "network---" + str);
-                    }
+            Log.i("间隔", "请求执行完成");
+
+            try {
+                if (is != null) {
+                    isc = new InputSource(is);
+
+                    Log.e("IS", "IS转换完成");
+                }else {
+                    Log.e("is", "is为空");
                 }
 
-            });
+
+                reader.parse(isc);
 
 
-//            reader.
-
-
-            // is = new InputSource(url.openStream());
-
-
-//            new Thread(){
-//                @Override
-//                public void run() {
-//                    try {
-//
-//                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//                        InputStream is = conn.getInputStream();
-//
-//                        InputSource isc = new InputSource(is);
-//
-//                        reader.parse(isc);
-//
-//                    }catch (IOException e){
-//                        Log.e("io", e.toString());
-//                    }catch (SAXException e){
-//                        Log.e("SAX", e.toString());
-//                    }
-//                }
-//            }.start();
-
-
+                Log.e("saxJiexi", "chenggong");
+            } catch (SAXException e) {
+                Log.i("SAX", e.toString());
+            }
             return rssHander.getFeed();
-
 
         } catch (Exception e) {
             Log.e("e", e.toString());
