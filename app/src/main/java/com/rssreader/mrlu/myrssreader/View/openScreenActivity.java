8f6e2f42cd71c9aefb4a4416d690e671e1c740f1
LuXin <lxxlxx888@163.com>
2017-04-16 22:49:51 +0800

    更新（2017.4.16） 更新内容： 1. 完成引导页 2. 对初次启动的逻辑进一步完善 3. 等

--- app/src/main/java/com/rssreader/mrlu/myrssreader/View/openScreenActivity.java
+++ app/src/main/java/com/rssreader/mrlu/myrssreader/View/openScreenActivity.java
@@ -1,7 +1,9 @@
 package com.rssreader.mrlu.myrssreader.View;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
@@ -24,21 +26,40 @@ public class openScreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_open_screen);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SystemClock.sleep(2000);
+        //从sp文件中取出isHasFeed,判断是否显示初始页
+        SharedPreferences sharedPreferences = getSharedPreferences("sp", Context.MODE_PRIVATE);
+        boolean isHasFeed = sharedPreferences.getBoolean("isHasFeed", false);
 
-                Intent intent = new Intent(openScreenActivity.this, LoginActivity.class);
+        if (isHasFeed) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    SystemClock.sleep(2000);
 
-                startActivity(intent);
+                    Intent intent = new Intent(openScreenActivity.this, LoginActivity.class);
 
-                finish();
+                    startActivity(intent);
+
+                    finish();
+                }
             }
-        }).start();
 
+            ).start();
 
+        } else {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    SystemClock.sleep(2000);
 
+                    Intent intent = new Intent(openScreenActivity.this, AppearPageActivity.class);
 
+                    startActivity(intent);
+
+                    finish();
+                }
+
+            }).start();
+        }
     }
 }
