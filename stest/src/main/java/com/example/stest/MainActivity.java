18db098db43c5eb77ed00436e0e1180f8af73957
LuXin <lxxlxx888@163.com>
2017-05-07 23:40:16 +0800

    更新（2017.5.7） 更新内容： 1. 添加StatusBarUtil，来设置标题栏沉浸 2. 测试DrawerLayout，试图添加侧滑栏

---
+++ stest/src/main/java/com/example/stest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.stest;
+
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.dl_test);
+
+        TextView text = (TextView) findViewById(R.id.tv_appear);
+
+
+    }
+}
