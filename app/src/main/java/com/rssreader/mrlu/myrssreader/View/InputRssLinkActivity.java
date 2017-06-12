35d8529396ca76531173d4f107971272fb6672b3
LuXin <lxxlxx8888@outlook.com>
2017-06-12 19:49:48 +0800

    更新（2017.6.12） 更新内容: 1. 叕次优化代码

--- app/src/main/java/com/rssreader/mrlu/myrssreader/View/InputRssLinkActivity.java
+++ app/src/main/java/com/rssreader/mrlu/myrssreader/View/InputRssLinkActivity.java
@@ -3,10 +3,12 @@ package com.rssreader.mrlu.myrssreader.View;
 import android.database.SQLException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.rssreader.mrlu.myrssreader.Model.Sqlite.SQLiteHandle;
 import com.rssreader.mrlu.myrssreader.R;
@@ -14,22 +16,20 @@ import com.rssreader.mrlu.myrssreader.R;
 public class InputRssLinkActivity extends AppCompatActivity {
 
 
-    EditText etRssLink;
-    Button btnOk;
+    private EditText mEtRssLink;
+    private ImageView mIvRssSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_rss_link);
+        initView();
 
-        etRssLink = (EditText) findViewById(R.id.et_rssLink);
-        btnOk = (Button) findViewById(R.id.btn_done);
-
-        btnOk.setOnClickListener(new View.OnClickListener() {
+        mIvRssSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String link = etRssLink.getText().toString();
+                String link = mEtRssLink.getText().toString();
 
                 Log.i("rssLink打印", link);
 
@@ -46,7 +46,6 @@ public class InputRssLinkActivity extends AppCompatActivity {
 //                    sqLiteHandle = new SQLiteHandle(InputRssLinkActivity.this);
 //                    sqLiteHandle.queryAllFeeds("AllFeeds");
 //                    sqLiteHandle.dbClose();
-
 
 
                 } catch (SQLException e) {
@@ -71,4 +70,21 @@ public class InputRssLinkActivity extends AppCompatActivity {
     }
 
 
+    private void initView() {
+        mEtRssLink = (EditText) findViewById(R.id.et_rssLink);
+        mIvRssSearch = (ImageView) findViewById(R.id.iv_rss_search);
+    }
+
+    private void submit() {
+        // validate
+        String rssLink = mEtRssLink.getText().toString().trim();
+        if (TextUtils.isEmpty(rssLink)) {
+            Toast.makeText(this, "请输入rss", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
+    }
 }
