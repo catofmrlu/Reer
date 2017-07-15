4b14d7707a2d10fd79498de27a9dd08f79f2dee7
Mr Lu <lxxlxx888@163.com>
2017-07-16 00:37:00 +0800

    更新（2017.7.15） 更新内容： 1. 优化UI部分引导页下方轮转指示显示

--- app/src/main/java/com/rssreader/mrlu/myrssreader/View/mainView.java
+++ app/src/main/java/com/rssreader/mrlu/myrssreader/View/mainView.java
@@ -2,6 +2,7 @@ package com.rssreader.mrlu.myrssreader.View;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.hardware.camera2.CaptureResult;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,6 +12,7 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.MAppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -307,6 +309,7 @@ public class mainView extends MAppCompatActivity implements View.OnClickListener
         }
 
     }
+
 }
 
 
