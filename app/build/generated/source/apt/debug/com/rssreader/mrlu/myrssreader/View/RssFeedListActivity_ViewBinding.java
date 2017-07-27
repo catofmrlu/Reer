d3715436247353cf5aa93deca3a0f6b084089160
Mr Lu <lxxlxx888@163.com>
2017-07-27 12:17:09 +0800

    更新游戏连接

---
+++ app/build/generated/source/apt/debug/com/rssreader/mrlu/myrssreader/View/RssFeedListActivity_ViewBinding.java
@@ -0,0 +1,35 @@
+// Generated code from Butter Knife. Do not modify!
+package com.rssreader.mrlu.myrssreader.View;
+
+import android.support.annotation.CallSuper;
+import android.support.annotation.UiThread;
+import android.view.View;
+import butterknife.Unbinder;
+import butterknife.internal.Utils;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+import com.rssreader.mrlu.myrssreader.Controller.RssFeedListActivity;
+import com.rssreader.mrlu.myrssreader.R;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+
+public class RssFeedListActivity_ViewBinding<T extends RssFeedListActivity> implements Unbinder {
+  protected T target;
+
+  @UiThread
+  public RssFeedListActivity_ViewBinding(T target, View source) {
+    this.target = target;
+
+    target.lvRssList = Utils.findRequiredViewAsType(source, R.id.lv_rssList, "field 'lvRssList'", SwipeMenuListView.class);
+  }
+
+  @Override
+  @CallSuper
+  public void unbind() {
+    T target = this.target;
+    if (target == null) throw new IllegalStateException("Bindings already cleared.");
+
+    target.lvRssList = null;
+
+    this.target = null;
+  }
+}
