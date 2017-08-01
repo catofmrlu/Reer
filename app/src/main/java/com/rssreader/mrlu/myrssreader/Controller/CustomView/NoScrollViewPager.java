05ee6bf712ea2cc2de05a618855188ee28242b2b
Mr Lu <lxxlxx888@163.com>
2017-08-02 01:05:11 +0800

    更新（2017.8.1） 更新内容： 1. 重新写了NoScrollViewPager

--- app/src/main/java/com/rssreader/mrlu/myrssreader/Controller/CustomView/NoScrollViewPager.java
+++ app/src/main/java/com/rssreader/mrlu/myrssreader/Controller/CustomView/NoScrollViewPager.java
@@ -2,6 +2,7 @@ package com.rssreader.mrlu.myrssreader.Controller.CustomView;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 
 /**
@@ -11,22 +12,22 @@ import android.view.MotionEvent;
 public class NoScrollViewPager  extends ViewPager{
 
 
-    private boolean isCanScroll = true;
-
     public NoScrollViewPager(Context context) {
         super(context);
     }
 
-    public void setIsScroll(boolean isCanScroll) {
-        this.isCanScroll = isCanScroll;
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent arg0) {
-        if(isCanScroll){
-            return super.onInterceptTouchEvent(arg0);
-        }else{
-            //false  不能左右滑动
-            return false;
-        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
     }
 }
