cbbb19a3803d20bff2701a1e0d81ff88832a5b43
Mr Lu <lxxlxx888@163.com>
2017-08-24 23:44:11 +0800

    更新（2017.8.24） 更新内容： 1. 添加inputstream转化为string部分，以便调试

--- app/src/main/java/com/rssreader/mrlu/myrssreader/Model/Rss/RssHanderByPull.java
+++ app/src/main/java/com/rssreader/mrlu/myrssreader/Model/Rss/RssHanderByPull.java
@@ -5,7 +5,10 @@ import android.util.Log;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -20,24 +23,46 @@ public class RssHanderByPull {
     private RSSItem mItem;
     private List<RSSItem> mRssList;
 
-    public  RSSFeed parseRss(String rssUrl){
+    public RSSFeed parseRss(String rssUrl) {
 
         HttpURLConnection conn = null;
+
         try {
             URL url = new URL(rssUrl);
             conn = (HttpURLConnection) url.openConnection();
 
             conn.setDoInput(true);
             conn.connect();
+
             InputStream is = conn.getInputStream();
             Log.i("is流获取", "success!!");
 
             PullParser(is);
 
-        }catch (Exception e){
-            Log.e("转化url", e.getMessage());
-        }finally {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
 
+            String line = null;
+
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.i("is转化为string", sb.toString());
+
+
+        } catch (Exception e) {
+            Log.e("转化url-error", e.getMessage());
+        } finally {
             conn.disconnect();
             conn = null;
         }
@@ -45,7 +70,7 @@ public class RssHanderByPull {
         return mFeed;
     }
 
-    public void PullParser(InputStream is){
+    public void PullParser(InputStream is) {
 
         try {
             // 使用工厂类XmlPullParserFactory
@@ -57,11 +82,11 @@ public class RssHanderByPull {
             parser.setInput(is, "UTF-8");
             int eventType = parser.getEventType();
             // 只要不是文档结束事件，就一直循环
-            while (eventType != XmlPullParser.END_DOCUMENT){
+            while (eventType != XmlPullParser.END_DOCUMENT) {
 
                 Log.i("xml解析", "进入开始节点！！");
 
-                switch (eventType){
+                switch (eventType) {
                     case XmlPullParser.START_DOCUMENT:
                         mFeed = new RSSFeed();
                         Log.i("xml解析", "进入START_DOCUMENT！！");
@@ -73,13 +98,17 @@ public class RssHanderByPull {
 
                         Log.i("xml解析", itemStartName + "！！");
 
-                        switch (itemStartName){
+                        switch (itemStartName) {
                             case "title":
+                                Log.i("xml解析", "title！！");
+
                                 if (mItem == null)
                                     mFeed.setName(parser.nextText());
                                 mItem.setTitle(parser.nextText());
                                 break;
                             case "link":
+                                Log.i("xml解析", "link！！");
+
                                 if (mItem == null)
                                     mFeed.setFeedLink(parser.nextText());
                                 mItem.setLink(parser.nextText());
@@ -107,7 +136,7 @@ public class RssHanderByPull {
                     case XmlPullParser.END_TAG:
 
                         String itemEndItem = parser.getName();
-                        if (itemEndItem == "item"){
+                        if (itemEndItem == "item") {
                             mFeed.addItem(mItem);
                             mItem = null;
                         }
@@ -116,7 +145,7 @@ public class RssHanderByPull {
             }
 
 
-        }catch (Exception e){
+        } catch (Exception e) {
             Log.e("PullParser方法部分", e.getMessage());
         }
     }
